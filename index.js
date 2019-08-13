a0c2f085df6f0ad188cf989734d4a937aceac3ba
github username <github email address>
2019-08-13 10:39:37 -0400

    done:

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// function totalBatteries(batteries) {
+//     let totalBatteries = 0;
+//     const reducer = (totalBatteries, currentValue) => totalBatteries + currentValue
+
+//     // totalBatteries = batteryBatches.reduce(reducer)
+
+//     // return totalBatteries
+//     return batteryBatches.reduce(reducer)
+// }
+
+const totalBatteries = batteryBatches.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+  }, 0);
+  
+
+console.log(totalBatteries(batteryBatches))
